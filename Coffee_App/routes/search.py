3b669b0750e92712b13c41bb8e4a2dd251f6483f
fussan <fujiisan1886@gmail.com>
2026-08-15 13:46:14 +0900

    コードの整形

--- Coffee_App/routes/search.py
+++ Coffee_App/routes/search.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, request, render_template
 
+search_bp = Blueprint("search", __name__)
 
-search_bp = Blueprint('search', __name__)
-@search_bp.route('/search')
+
+@search_bp.route("/search")
 def search():
-  return '検索ページ'
+    return "検索ページ"
