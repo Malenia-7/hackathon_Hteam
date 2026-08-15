93b99493d857f5aff1bd5c644eda5df4e3a3ebca
fujiieiwa <fujiisan1886@gmail.com>
2026-08-15 19:09:29 +0900

    検索機能の追加

--- Coffee_App/routes/search.py
+++ Coffee_App/routes/search.py
@@ -1,3 +1,4 @@
+from Coffee_App.models.comment import Comment
 from flask import Blueprint, request, render_template
 
 search_bp = Blueprint("search", __name__)
@@ -5,9 +6,11 @@ search_bp = Blueprint("search", __name__)
 
 @search_bp.route("/search")
 def search():
-    keyword = request.args.get("q")
+    keyword = request.args.get("q", "")
 
-    if keyword is None:
-        return "キーワードがありません"
+    if keyword:
+        comments = Comment.query.filter(Comment.content.like(f"%{keyword}%")).all()
     else:
-        return keyword
+        comments = []
+
+    return render_template("search/search.html", comments=comments, keyword=keyword)
