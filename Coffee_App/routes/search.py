88c39edb82fcafa0fd7b35b018f6b9902ee5f0d0
fussan <fujiisan1886@gmail.com>
2026-08-15 14:30:54 +0900

    検索機能の追加

--- Coffee_App/routes/search.py
+++ Coffee_App/routes/search.py
@@ -5,4 +5,9 @@ search_bp = Blueprint("search", __name__)
 
 @search_bp.route("/search")
 def search():
-    return "検索ページ"
+  keyword = request.args.get("q")
+  
+  if keyword is None:
+    return "キーワードがありません"
+  else:
+    return keyword  
