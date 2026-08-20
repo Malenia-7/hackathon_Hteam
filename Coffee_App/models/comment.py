d616f75df1c3a791fa36458fa89b7e4c5969757b
fujiieiwa <fujiisan1886@gmail.com>
2026-08-20 22:16:05 +0900

    コメントの変更と機能の削除

--- Coffee_App/models/comment.py
+++ Coffee_App/models/comment.py
@@ -1,8 +1,8 @@
 # コメント一覧の取得
-SELECT * comments  WHERE post_id=%s;
+SELECT * FROM comments  WHERE post_id=%s;
 
 # コメントの削除
 DELETE FROM comments WHERE post_id=%s;
 
-# コメントの追加
+# コメントの投稿
 INSERT INTO comments (content, user_id, post_id) VALUES (%s, %s, %s);
--- Coffee_App/models/reaction.py
+++ Coffee_App/models/reaction.py
@@ -1,2 +1,2 @@
-from ..extensions import db
+
 
