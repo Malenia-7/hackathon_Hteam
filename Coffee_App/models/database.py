df0220418d79ed46cf6c450a0aa0c4656a13a97b
fujiieiwa <fujiisan1886@gmail.com>
2026-08-21 22:38:43 +0900

    データベース接続処理を追加

---
+++ Coffee_App/models/database.py
@@ -0,0 +1,12 @@
+import os
+import pymysql
+
+
+def get_db_connection():
+    return pymysql.connect(
+        host="db",
+        user=os.getenv("MYSQL_USER"),
+        password=os.getenv("MYSQL_PASSWORD"),
+        database=os.getenv("MYSQL_DATABASE"),
+        port=3306,
+    )
