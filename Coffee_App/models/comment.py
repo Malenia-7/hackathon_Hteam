75e4823618292de9497ade79335e63349a693d39
fujiieiwa <fujiisan1886@gmail.com>
2026-08-21 22:38:05 +0900

    コメントの取得・投稿・削除処理を追加

--- Coffee_App/models/comment.py
+++ Coffee_App/models/comment.py
@@ -1,8 +1,41 @@
+from Coffee_App.models.database import get_db_connection
+
+
 # コメント一覧の取得
-SELECT * FROM comments  WHERE post_id=%s;
+def get_comments(post_id):
+    conn = get_db_connection()
+    cr = conn.cursor()
+
+    sql = """SELECT * FROM comments  WHERE post_id=%s;"""
+    cr.execute(sql, (post_id,))
+    comments = cr.fetchall()
+
+    cr.close()
+    conn.close()
+    return comments
+
 
 # コメントの削除
-DELETE FROM comments WHERE post_id=%s;
+def delete_comments(comment_id):
+    conn = get_db_connection()
+    cr = conn.cursor()
+
+    sql = """DELETE FROM comments WHERE id=%s;"""
+    cr.execute(sql, (comment_id,))
+    conn.commit()
+
+    cr.close()
+    conn.close()
+
 
 # コメントの投稿
-INSERT INTO comments (content, user_id, post_id) VALUES (%s, %s, %s);
+def post_comments(content, user_id, post_id):
+    conn = get_db_connection()
+    cr = conn.cursor()
+
+    sql = """INSERT INTO comments (content, user_id, post_id) VALUES (%s, %s, %s);"""
+    cr.execute(sql, (content, user_id, post_id))
+    conn.commit()
+
+    cr.close()
+    conn.close()
