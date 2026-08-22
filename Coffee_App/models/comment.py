17fcf65a30c7fe857541f2849e8cd54926fc62ba
fujiieiwa <fujiisan1886@gmail.com>
2026-08-22 19:05:31 +0900

    コメントDB操作にエラー処理を追加

--- Coffee_App/models/comment.py
+++ Coffee_App/models/comment.py
@@ -3,16 +3,19 @@ from Coffee_App.models.database import get_db_connection
 
 # コメント一覧の取得
 def get_comments(post_id):
+
     conn = get_db_connection()
     cr = conn.cursor()
 
-    sql = """SELECT * FROM comments  WHERE post_id=%s;"""
-    cr.execute(sql, (post_id,))
-    comments = cr.fetchall()
+    try:
+        sql = """SELECT * FROM comments  WHERE post_id=%s;"""
+        cr.execute(sql, (post_id,))
+        comments = cr.fetchall()
+        return comments
 
-    cr.close()
-    conn.close()
-    return comments
+    finally:
+        cr.close()
+        conn.close()
 
 
 # コメントの削除
@@ -20,12 +23,18 @@ def delete_comments(comment_id):
     conn = get_db_connection()
     cr = conn.cursor()
 
-    sql = """DELETE FROM comments WHERE id=%s;"""
-    cr.execute(sql, (comment_id,))
-    conn.commit()
+    try:
+        sql = """DELETE FROM comments WHERE id=%s;"""
+        cr.execute(sql, (comment_id,))
+        conn.commit()
 
-    cr.close()
-    conn.close()
+    except Exception:
+        conn.rollback()
+        raise
+
+    finally:
+        cr.close()
+        conn.close()
 
 
 # コメントの投稿
@@ -33,9 +42,17 @@ def post_comments(content, user_id, post_id):
     conn = get_db_connection()
     cr = conn.cursor()
 
-    sql = """INSERT INTO comments (content, user_id, post_id) VALUES (%s, %s, %s);"""
-    cr.execute(sql, (content, user_id, post_id))
-    conn.commit()
+    try:
+        sql = (
+            """INSERT INTO comments (content, user_id, post_id) VALUES (%s, %s, %s);"""
+        )
+        cr.execute(sql, (content, user_id, post_id))
+        conn.commit()
 
-    cr.close()
-    conn.close()
+    except Exception:
+        conn.rollback()
+        raise
+
+    finally:
+        cr.close()
+        conn.close()
