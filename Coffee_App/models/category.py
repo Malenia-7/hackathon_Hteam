ffd7774054b6d7e39cadc36aae61ad54569162a0
fujiieiwa <fujiisan1886@gmail.com>
2026-08-22 19:22:40 +0900

    カテゴリの取得処理を追加

--- Coffee_App/models/category.py
+++ Coffee_App/models/category.py
@@ -1,5 +1,33 @@
+from Coffee_App.models.database import get_db_connection
+
+
 # カテゴリ一覧を取得
-SELECT category_name FROM categories;
+def get_categories():
+    conn = get_db_connection()
+    cr = conn.cursor()
+
+    try:
+        sql = """SELECT category_name FROM categories;"""
+        cr.execute(sql)
+        categories = cr.fetchall()
+
+        return categories
+    finally:
+        cr.close()
+        conn.close()
+
 
 # 特定のカテゴリを取得
-SELECT category_name FROM categories WHERE id=%s;
+def get_category(category_id):
+    conn = get_db_connection()
+    cr = conn.cursor()
+
+    try:
+        sql = """SELECT category_name FROM categories WHERE id=%s;"""
+        cr.execute(sql, (category_id,))
+        category_name = cr.fetchone()
+
+        return category_name
+    finally:
+        cr.close()
+        conn.close()
