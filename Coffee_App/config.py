c55786789c8fc059f231d6ba5680df8b8a1a9bdb
yteruya_s296 <yteruya_s296@cruxsystem.co.jp>
2026-08-26 20:25:44 +0900

    SQLALCHEMYの削除

--- Coffee_App/config.py
+++ Coffee_App/config.py
@@ -3,12 +3,14 @@ from pathlib import Path
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = (
-        f"mysql+pymysql://"
-        f"{os.getenv('MYSQL_USER')}:"
-        f"{os.getenv('MYSQL_PASSWORD')}@"
-        f"db:3306/"
-        f"{os.getenv('MYSQL_DATABASE')}"
-    )
+    SECRET_KEY = os.getenv("SECRET_KEY")
 
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MYSQL_HOST = os.getenv("MYSQL_HOST", "db")
+    MYSQL_PORT = int(os.getenv("MYSQL_PORT", "3306"))
+    MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
+    MYSQL_USER = os.getenv("MYSQL_USER")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+    MYSQL_CHARSET = "utf8mb4"
+
+    POST_UPLOAD_FOLDER = Path(__file__).parent / "static" / "uploads" / "posts"
+    ALLOWED_IMAGE_EXTENSIONS = {"jpg", "jpeg", "png", "webp"}
